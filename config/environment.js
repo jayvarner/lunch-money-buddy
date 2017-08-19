64539c301fa8de5b207fb77366990b59c2c81094
Jay Varner <jayvarner@gmail.com>
2017-08-18 23:35:36 -0400

    hmmm

--- config/environment.js
+++ config/environment.js
@@ -27,6 +27,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.API_HOST = 'http://192.168.200.128:3000';
   }
 
   if (environment === 'test') {
